49ed6d8d5ce8cf8ed6b3d335cf52e9f8489709b2
vishwaak <chandranvishwaak@gmail.com>
2025-02-12 01:36:30 -0600

    chore: reformed the code

--- catkin_ws/src/cv_basics/scripts/haptic_node.py
+++ catkin_ws/src/cv_basics/scripts/haptic_node.py
@@ -11,52 +11,50 @@ from kairos_laser.msg import obstacle
 
 
 class haptic_node:
-    def __init__  (self):
-        rospy.init_node('haptic_node', anonymous=True)
+    def __init__(self):
+        rospy.init_node("haptic_node", anonymous=True)
         self.haptic = haptic_controller()
         self.rate = rospy.Rate(100)
 
         self.force_step = 1
         self.min_dst = 7
-        self.max_dst =  15   
+        self.max_dst = 15
         self.scaleL = 50
         self.scaleR = 255
         self.force_func = 2
 
-        # self.dst_dirc = Queue()
-
-        
-
-        self.haptic_direction = "vib" #vib or braille
+        self.haptic_direction = "vib"  # vib or braille
         self.curr_dst = 0.0
         self.dirc = 0.0
 
-        self.magnitude_data = rospy.Subscriber('/obstacle_info', obstacle, self.distance_callback, queue_size=1)
+        self.magnitude_data = rospy.Subscriber(
+            "/obstacle_info", obstacle, self.distance_callback, queue_size=1
+        )
         self.haptic_thread = threading.Thread(target=self.run_node)
+
     def norm_x(self, x):
         norm = self.max_dst - self.min_dst
-        x = ((self.max_dst - x) / norm)
+        x = (self.max_dst - x) / norm
         return x
-    
+
     def haptic_force(self, x):
         if self.force_func == 1:
             slope = -1 / (self.max_dst - self.min_dst)
-            y  = slope * (x - self.min_dst) + 1
+            y = slope * (x - self.min_dst) + 1
             return y
         elif self.force_func == 2:
-            y = self.norm_x(x)**2
+            y = self.norm_x(x) ** 2
             return y
 
-        elif self.force_func ==3:
+        elif self.force_func == 3:
             x = self.norm_x(x)
-            y = 1/math.exp((x**2)*math.pi)
+            y = 1 / math.exp((x**2) * math.pi)
             return y
 
         elif self.force_func == 4:
-            x =  self.norm_x(x)
+            x = self.norm_x(x)
             y = -1 * math.log10(x)
             return y
-   
 
     def get_force(self, x):
         if x < self.min_dst:
@@ -66,92 +64,102 @@ class haptic_node:
         else:
             force = self.haptic_force(x)
 
-        return max(0,force) if force < 1 else 1
-    
+        return max(0, force) if force < 1 else 1
+
     def directional(self, angle):
-        
+
         input_angle = math.radians(angle)
-        right_haptic = 0.5 + (math.sin(input_angle))/2
-        left_haptic = 0.5 + (math.sin(-input_angle))/2
+        right_haptic = 0.5 + (math.sin(input_angle)) / 2
+        left_haptic = 0.5 + (math.sin(-input_angle)) / 2
         return left_haptic, right_haptic
-    
-    
+
     def haptic_feeback(self, distance, direction):
         force = self.get_force(distance)
-        print("force :",force)
+        print("force :", force)
         if self.haptic_direction == "vib":
             left, right = self.directional(direction)
         else:
             return force, force
-        
+
         right = right * force
         left = left * force
 
         return left, right
-    
+
     def distance_callback(self, data):
         distance = data.distance.data
         direction = data.direction.data
-        header  =  data.header
-        # print("data", data)
+
         distance = distance * 10
-       
+
         if self.max_dst > distance and distance > self.min_dst:
-          
+
             self.curr_dst = distance
             self.dirc = direction
         else:
             self.curr_dst = 0
             self.dirc = 0
-            # selself.haptic_thread = threading.Thread(target=self.run_node, args=(self.curr_dst,self.dirc))f.dst_dirc.put((distance, direction, header))
-    
+
+
     def vibrate_pattern(self, forceR, forceL, dir):
-        
+
         if dir > 270 and dir <= 360:
-            self.haptic.vibrate_pattern(left=forceL, right=forceR,time=[0.09,0.09], count=1)
+            self.haptic.vibrate_pattern(
+                left=forceL, right=forceR, time=[0.09, 0.09], count=1
+            )
         if dir >= 0 and dir <= 90:
-            self.haptic.vibrate_pattern(left=forceL, right=forceR,time=[0.09,0.09], count=1)
-            self.haptic.vibrate_pattern(left=0, right=forceR,time=[0.09], count=1)
-            self.haptic.vibrate_pattern(left=forceL, right=forceR,time=[0.09], count=1)
+            self.haptic.vibrate_pattern(
+                left=forceL, right=forceR, time=[0.09, 0.09], count=1
+            )
+            self.haptic.vibrate_pattern(left=0, right=forceR, time=[0.09], count=1)
+            self.haptic.vibrate_pattern(left=forceL, right=forceR, time=[0.09], count=1)
         if dir > 90 and dir <= 180:
-            self.haptic.vibrate_pattern(left=forceL, right=forceR,time=[0.09,0.09], count=1)
+            self.haptic.vibrate_pattern(
+                left=forceL, right=forceR, time=[0.09, 0.09], count=1
+            )
         if dir > 180 and dir <= 270:
-            self.haptic.vibrate_pattern(left=forceL, right=forceR,time=[0.09,0.09, 0.09], count=1)
-    
+            self.haptic.vibrate_pattern(
+                left=forceL, right=forceR, time=[0.09, 0.09, 0.09], count=1
+            )
+
     def run_node(self):
-        
+
         while not rospy.is_shutdown():
-            # dst, dirc, header = dst_dirc.get()
+          
             dst = self.curr_dst
             dirc = self.dirc
-            
+
             print("distance", dst, "direction", dirc)
 
             left_haptic, right_haptic = self.haptic_feeback(dst, dirc)
-            
-            print("left",left_haptic*self.scaleL,"right",right_haptic*self.scaleR)
+
+            print(
+                "left", left_haptic * self.scaleL, "right", right_haptic * self.scaleR
+            )
 
             if self.haptic_direction == "vib":
-                self.haptic.vibrate_both(intensityL=math.floor(left_haptic*self.scaleL), intensityR=math.floor(right_haptic*self.scaleR))
+                self.haptic.vibrate_both(
+                    intensityL=math.floor(left_haptic * self.scaleL),
+                    intensityR=math.floor(right_haptic * self.scaleR),
+                )
             else:
-                self.vibrate_pattern(math.floor(left_haptic*self.scaleL), math.floor(right_haptic*self.scaleR), dirc)
-            
+                self.vibrate_pattern(
+                    math.floor(left_haptic * self.scaleL),
+                    math.floor(right_haptic * self.scaleR),
+                    dirc,
+                )
+
             sleep(0.03)
 
-            # if dst < self.max_dst and self.min_dst < dst:
-            #     sleep(0.03) #0.03
-            # else:
-            #     self.haptic.reset_hacptic()
-            #     sleep(0.01)
-                
+           
+
         haptic_controller.close()
-    
+
     def start_node(self):
         self.haptic_thread.start()
-        
-            
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     node = haptic_node()
     try:
         node.start_node()
--- catkin_ws/src/cv_basics/scripts/sound_node.py
+++ catkin_ws/src/cv_basics/scripts/sound_node.py
@@ -14,6 +14,7 @@ import threading
 
 from time import sleep
 
+
 class sound_node:
 
     def __init__(self):
@@ -21,8 +22,8 @@ class sound_node:
         self.min_dst = 2
         self.max_dst = 30
         self.mid_dst = (self.min_dst + self.max_dst) / 2
-        rospy.init_node('sound_node', anonymous=True)
-        self.sound_dict ={
+        rospy.init_node("sound_node", anonymous=True)
+        self.sound_dict = {
             "near": "sounds/near.wav",
             "far": "sounds/far.wav",
             "left": "sounds/left.wav",
@@ -33,18 +34,22 @@ class sound_node:
             "human": "sounds/human.wav",
         }
 
-        self.obsatcle_type = rospy.Subscriber('/object_type', object_type, self.play_sound)
-        self.obstacle_info = rospy.Subscriber('/obstacle_info', obstacle, self.play_sound)
+        self.obsatcle_type = rospy.Subscriber(
+            "/object_type", object_type, self.play_sound
+        )
+        self.obstacle_info = rospy.Subscriber(
+            "/obstacle_info", obstacle, self.play_sound
+        )
 
-        self.rate = rospy.Rate(100) 
+        self.rate = rospy.Rate(100)
 
         self.obs_info = None
         self.obs_type = None
 
         self.lock = threading.Lock()
 
-#info contains the distance
-#type contains the type of object
+    # info contains the distance
+    # type contains the type of object
 
     def refresh_rate(self, x):
         limit = ((x - self.min_dst) / (self.max_dst - self.min_dst)) * 100
@@ -56,18 +61,16 @@ class sound_node:
             return 2
         else:
             return 2
-    
 
     def play_sound(self, data):
         with self.lock:
             if isinstance(data, obstacle):
                 self.obs_info = data
             elif isinstance(data, object_type):
-                self.obs_type= data
+                self.obs_type = data
             if self.obs_info and self.obs_type:
                 self.choose_sound(self.obs_info, self.obs_type)
 
-    
     def get_dir_sound(self, direction):
         if direction > 180 and direction <= 270:
             return self.sound_dict["back"]
@@ -80,11 +83,9 @@ class sound_node:
         else:
             return None
 
-            
-
     def choose_sound(self, obstacle_info, obstacle_type):
 
-        distance = obstacle_info.distance.data * 10 
+        distance = obstacle_info.distance.data * 10
         direction = obstacle_info.direction.data
 
         print("Distance", distance)
@@ -95,17 +96,17 @@ class sound_node:
             obs_type = "box"
         else:
             obs_type = "none"
-        
-        sounds_play = []
-        
-        #approaching obstacle on left
-        #warning left
 
-        #TODO: add more directions
-        #TODO: Help direction if ideal for more time spot the free direction.
-        #TODO: use closed obstacle to determine the direction or go fancy and use the camera
-        #TODO: Ask how many times they need assitance
-        #TODO: set common parameters
+        sounds_play = []
+
+        # approaching obstacle on left
+        # warning left
+
+        # TODO: add more directions
+        # TODO: Help direction if ideal for more time spot the free direction.
+        # TODO: use closed obstacle to determine the direction or go fancy and use the camera
+        # TODO: Ask how many times they need assitance
+        # TODO: set common parameters
 
         if distance < self.max_dst:
             if self.min_dst < distance and distance < self.mid_dst:
@@ -118,19 +119,18 @@ class sound_node:
 
         print("Sounds to play", sounds_play)
 
-
         if len(sounds_play) > 1:
             for sounds in sounds_play:
                 filename = sounds
                 base_path = os.path.dirname(os.path.abspath(__file__))
                 filename = os.path.join(base_path, filename)
-                data, fs = sf.read(filename, dtype='float32')
+                data, fs = sf.read(filename, dtype="float32")
                 print("Playing sound")
                 sd.play(data, fs)
                 sleep(self.refresh_rate(distance))
 
 
 if __name__ == "__main__":
-    
+
     sound_node()
     rospy.spin()
